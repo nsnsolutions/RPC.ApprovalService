3172d20b3aa25cba8c9383e6db5c570aad35258b
Ian Laird <irlaird@gmail.com>
2017-08-16 10:57:16 -0600

    WIP: Started converting bootstrap code to use RDS - All plugins are broken.

--- src/index.js
+++ src/index.js
@@ -1,30 +1,33 @@
 'use strict';
 
 const AWS = require('aws-sdk');
-const redis = require('redis');
+const Bookshelf = require('bookshelf');
+const Knex = require('knex');
+const Redis = require('redis');
+const SchemaInstaller = require('knex-schema-builder');
+const async = require('async');
+const schemaPath = require('path').join(__dirname, './database')
 const rpcUtils = require('rpc-utils');
-const services = require('./services');
+
 const lib = require('./lib');
+const services = require('./services');
 
 module.exports = function RPC_ApprovalService(App) {
 
-    // Simplify reference to configurations.
-    var conf = App.configurations;
+    let redisClient, knexClient;
+
+    var config = App.configurations;
 
     // Validate Shared Configs
-    conf.shared.assertMember("region");
-    conf.shared.assertMember("cacheEndpoint");
-    conf.shared.assertMember("logLevel");
-    conf.shared.assertMember("clusterName");
-
-    // Validate Service Configs.
-    conf.service.assertMember("approvalTableName");
+    config.shared.assertMember("region");
+    config.shared.assertMember("logLevel");
+    config.shared.assertMember('rdsUrl');
 
     var app = App({
         onConfigUpdate: () => app.restart(),
         onStart: bootstrap,
-        onRestart: bootstrap,
-        onShutdown:() => process.exit(0)
+        onRestart: restart,
+        onShutdown: shutdown,
     });
 
     app.start();
@@ -33,47 +36,142 @@ module.exports = function RPC_ApprovalService(App) {
 
     function bootstrap(bus, conf) {
 
-        var redisClient = redis.createClient({ url: conf.shared.cacheEndpoint });
-        var dynamoClient = new AWS.DynamoDB({ region: conf.shared.region });
-        var proxy = new lib.Proxy({ seneca: bus });
+        redisClient = Redis.createClient({ url: conf.shared.redisUrl });
+        redisClient.on('error', onRedisError);
 
-        var tableCacheParams = {
-            dynamoClient: dynamoClient,
-            redisClient: redisClient,
-            tableMap: {
-                approval: conf.service.approvalTableName 
-            }
-        };
+        knexClient = Knex({
+            client: conf.shared.rdsUrl.split(':').shift(),
+            connection: conf.shared.rdsUrl,
+            //debug: true
+        });
 
-        rpcUtils.CachedTable.create(tableCacheParams, (err, tables) => {
+        var startupTasks = [
+            (cb) => SchemaInstaller.setTablePrefix('approvalService_', (err) => cb(err)),
+            (cb) => initializeDatabase(schemaPath, cb),
+            (cb) => installPlugins(bus, conf, cb)
+        ];
+
+        async.waterfall(startupTasks, (err) => {
 
             if(err) {
-                console.error("FATAL: Failed to initialize dynamo cache.", err);
-                return process.exit(1);
+                console.error("FAILURE!", err);
+                throw err;
             }
 
-            var params = {
-                env: conf.shared.clusterName,
-                logLevel: conf.shared.logLevel,
-                tables: tables,
-                redisClient: redisClient,
-                dynamoClient: dynamoClient,
-                proxy: proxy
-            };
-
-            bus.use(services.ApprovalCreatePlugin, params);
-            bus.use(services.ApprovalMetricPlugin, params);
-            bus.use(services.ApprovalListPlugin, params);
-            bus.use(services.ApprovalFetchPlugin, params);
-            bus.use(services.ApprovalCompletePlugin, params);
-
+            // Allow outbound calls to everything.
             bus.rpcClient({ pin: "role:*" });
+
+            // These are the route keys that the RabbitMQ queue will subscribe to
+            // on the seneca rpc exchange. Only *.Pub keys are routed from
+            // rpc.interface. non .Pub routes are used for interal calls only.
+
             bus.rpcServer({ pin: [
                 "role:approvalService",
                 "role:approvalService.Pub"
             ]});
 
-        });
+            bus.on('close', () => {
+                console.log("[ X:APP ] Seneca bus is shutting down...");
+                bus.emit('stopConsuming');
+            });
 
+            bus.ready(() => {
+                console.log("[ X:APP ] Seneca bus is ready...");
+                setTimeout(() => bus.emit('startConsuming'), 3000);
+            });
+
+        });
     }
+
+    function shutdown() {
+        cleanUp();
+        process.exit(0);
+    }
+
+    function restart(bus, conf) {
+        cleanUp();
+        bootstrap(bus, conf);
+    }
+
+    function installPlugins(bus, conf, callback) {
+
+        var bookshelf = Bookshelf(knexClient);
+
+        var params = {
+            logLevel: conf.shared.logLevel,
+            redisClient: redisClient,
+            models: lib.Models(bookshelf),
+        };
+
+        //bus.use(services.ApprovalCreatePlugin, params);
+        //bus.use(services.ApprovalMetricPlugin, params);
+        //bus.use(services.ApprovalListPlugin, params);
+        //bus.use(services.ApprovalFetchPlugin, params);
+        //bus.use(services.ApprovalCompletePlugin, params);
+
+        callback();
+    }
+
+    function initializeDatabase(path, callback) {
+
+        console.info("[ X:APP ] Checking RDS Schema...");
+
+        SchemaInstaller.isInstallNeeded(knexClient, path, (err, needsInstall) => {
+
+            if(err)
+                return callback(err);
+
+            SchemaInstaller.isUpgradeNeeded(knexClient, path, (err, needsUpgrade) => {
+
+                if(needsInstall) {
+
+                    console.warn("[ X:APP ] Initializing RDS Database...");
+                    SchemaInstaller.install(knexClient, path, (err, result) => {
+
+                        if(err)
+                            return callback(err);
+
+                        console.warn("[ X:APP ] Initialization Complete");
+                        console.log(result);
+                        callback();
+                    });
+
+                } else if(needsUpgrade) {
+
+                    console.warn("[ X:APP ] Upgrading RDS Database...");
+                    SchemaInstaller.upgrade(knexClient, path, (err, result) => {
+
+                        if(err)
+                            return callback(err);
+
+                        console.warn("[ X:APP ] Upgrade Complete");
+                        console.log(result);
+                        callback();
+                    });
+
+                } else {
+
+                    callback();
+
+                }
+            });
+        });
+    }
+
+    function cleanUp() {
+        if(redisClient && redisClient.quit)
+            redisClient.quit();
+
+        if(knexClient && knexClient.destroy)
+            knexClient.destroy();
+    }
+
+    function onRedisError(err) {
+        try {
+            console.log("ERROR (REDIS)", JSON.stringify(err));
+        } catch (e) {
+            console.log("ERROR (REDIS)", err);
+        }
+    }
+
 }
